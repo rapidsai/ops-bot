aec5875251b01ffec2471f5b36e134f35b7880c6
Joseph <68436579+jolorunyomi@users.noreply.github.com>
2020-10-29 11:11:28 -0500

    init release drafter as well

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Application } from "probot";
 import { initLabelCheck } from "./plugins/LabelCheck/label_check";
+import releaseDrafter from "release-drafter-github-app";
 
 export = (app: Application) => {
   initLabelCheck(app);
+  releaseDrafter(app);
 };
