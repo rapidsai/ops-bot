9940bb06963ed567cc671f055a84093e3bea6ca7
AJ Schmidt <aschmidt@nvidia.com>
2020-10-29 20:59:35 -0400

    WIP, backup

--- auto-merger/src/main.ts
+++ auto-merger/src/main.ts
@@ -1,9 +1,23 @@
-import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
+import { Octokit } from "@octokit/rest";
+import { throttling } from "@octokit/plugin-throttling";
+import {
+  PullsGetResponseData,
+  IssuesListEventsForTimelineResponseData,
+  UsersGetByUsernameResponseData,
+} from "@octokit/types";
 import { createAppAuth } from "@octokit/auth-app";
-import * as strip from "strip-comments";
+import strip from "strip-comments";
 
-const privateKey = process.env.PRIVATE_KEY as string;
-type PullRequestData = RestEndpointMethodTypes["pulls"]["get"]["response"]["data"];
+const MyOctokit = Octokit.plugin(throttling).defaults({
+  per_page: 100,
+});
+
+// const privateKey = process.env.PRIVATE_KEY as string;
+
+interface Author {
+  name: string;
+  email: string;
+}
 
 // const mergeLabel = "okay to merge";
 const mergeLabel = "doc";
@@ -11,77 +25,167 @@ const org = "rapidsai";
 
 const buff = Buffer.from(privateKey, "base64");
 
-const client = new Octokit({
+const client = new MyOctokit({
   authStrategy: createAppAuth,
   auth: {
     appId: 85914,
     privateKey: buff.toString("ascii"),
     installationId: 12543261,
   },
+  throttle: {
+    onRateLimit: (retryAfter, options, octokit) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      );
+      octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+      return true;
+    },
+    onAbuseLimit: (retryAfter, options, octokit) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `Abuse detected for request ${options.method} ${options.url}`
+      );
+      octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+      return true;
+    },
+  },
 });
 
-const isPrMergeable = async (
-  pr: PullRequestData,
-  default_branch: string
-): Promise<boolean> => {
-  const repoName = pr.base.ref;
+/**
+ * Returns true if PR's checks are all passing and it is being
+ * merged into the default branch.
+ *
+ * @param pr
+ */
+const isPrMergeable = (pr: PullsGetResponseData): boolean =>
+  pr.mergeable_state === "clean" &&
+  pr.mergeable === true &&
+  pr.base.ref === pr.base.repo.default_branch;
 
-  // Checks if user who applied merge label has appropriate permissions
-  // to merge.
-  const mergeLabelActor = await getMergeLabelActor(pr);
+/**
+ * Determines if user who added merge label has permissions to merge
+ *
+ * @param pr
+ * @param events
+ */
+const hasValidMergeLabelActor = async (
+  pr: PullsGetResponseData,
+  events: IssuesListEventsForTimelineResponseData
+): Promise<boolean> => {
+  const repoName = pr.base.repo.name;
+
   const mergeUsers = (
     await client.paginate(client.teams.listMembersInOrg, {
       org,
       team_slug: `${repoName}-write`,
-      per_page: 100,
     })
   ).map((el) => el.login);
 
-  return (
-    pr.mergeable_state === "clean" &&
-    pr.mergeable === true &&
-    pr.base.ref === default_branch &&
-    mergeUsers.includes(mergeLabelActor)
-  );
-};
-
-// Determines the last user to add the merge label
-const getMergeLabelActor = async (pr: PullRequestData): Promise<string> => {
-  const events = await client.paginate(client.issues.listEventsForTimeline, {
-    owner: org,
-    repo: pr.base.repo.name,
-    issue_number: pr.number,
-    per_page: 100,
-  });
-
-  return events
+  const mergeLabelActor = events
     .filter((el: any) => el.event === "labeled" && el.label.name === mergeLabel)
     .sort((a, b) => +new Date(b.created_at) - +new Date(a.created_at))[0].actor
     .login;
+
+  return mergeUsers.includes(mergeLabelActor);
+};
+
+// Drop [] brackets from PR title
+const sanitizePrTitle = (rawTitle) => {
+  return rawTitle.replace(/\[[\s\S]*?\]/g, "").trim();
+};
+
+const getUniqueAuthors = (
+  events: IssuesListEventsForTimelineResponseData
+): Author[] => {
+  return events
+    .filter((el) => el.event === "committed")
+    .reduce((acc: any, cur: any) => {
+      const author = {
+        name: cur.author.name,
+        email: cur.author.email,
+      };
+      const alreadyInArray =
+        acc.findIndex((el) => JSON.stringify(author) === JSON.stringify(el)) !==
+        -1;
+
+      if (alreadyInArray) return acc;
+      return [...acc, author];
+    }, []);
+};
+
+const getApproverProfiles = async (
+  pr: PullsGetResponseData
+): Promise<UsersGetByUsernameResponseData[]> => {
+  const reviewers = await client.paginate(client.pulls.listReviews, {
+    owner: org,
+    repo: pr.base.repo.name,
+    pull_number: pr.number,
+  });
+  const approvers = reviewers.filter((review) => review.state === "APPROVED");
+
+  return Promise.all(
+    approvers.map(
+      async (approver) =>
+        (
+          await client.users.getByUsername({
+            username: approver.user.login,
+          })
+        ).data
+    )
+  );
+};
+
+const createCommitMessage = async (
+  pr: PullsGetResponseData,
+  events: IssuesListEventsForTimelineResponseData
+): Promise<string> => {
+  let commitMsg = "";
+
+  const prBody = strip(pr.body, {
+    language: "html",
+    preserveNewlines: false,
+  }).trim();
+  const authors = getUniqueAuthors(events);
+
+  const approvers = await getApproverProfiles(pr);
+
+  commitMsg += `${prBody}\n`;
+
+  commitMsg += "Authors:\n";
+  for (let i = 0; i < authors.length; i++) {
+    const author = authors[i];
+    commitMsg += `  - ${author.name} <${author.email}>\n`;
+  }
+  commitMsg += "Approvers:\n";
+  for (let j = 0; j < approvers.length; j++) {
+    const approver = approvers[j];
+    commitMsg += `  - ${approver.name}\n`;
+  }
+  commitMsg += `URL: ${pr.html_url}`;
+
+  return commitMsg;
 };
 
 export const mergePRs = async () => {
-  console.log("inside async");
   // Get repos with app installed
-  const { data } = await client.apps.listReposAccessibleToInstallation();
-  const { repositories } = data;
+  const repositories = await client.paginate(
+    client.apps.listReposAccessibleToInstallation
+  );
 
   // Iterate over repos
   for (let i = 0; i < repositories.length; i++) {
     const repo = repositories[i];
-    const { default_branch } = repo;
+    console.log(`starting repo - ${repo.full_name}`);
 
     // Query PRs with merge label
     const queryResults = await client.paginate(
       client.search.issuesAndPullRequests,
       {
         q: `repo:${repo.full_name} is:pr is:open label:"${mergeLabel}"`,
-        per_page: 100,
       }
     );
 
-    // Filter out PRs that shouldn't be merged
-    const prs: PullRequestData[] = [];
+    // Iterate over & merge PRs
     for (let j = 0; j < queryResults.length; j++) {
       const result = queryResults[j];
 
@@ -92,35 +196,49 @@ export const mergePRs = async () => {
         pull_number: result.number,
       });
 
-      if (!(await isPrMergeable(pr, default_branch))) {
+      console.log(`starting PR #${pr.number} - ${pr.title}`);
+      if (!(await isPrMergeable(pr))) {
+        console.log(
+          `PR #${pr.number} has conflicts or pending CI checks. Skipping...`
+        );
         continue;
       }
 
-      prs.push(pr);
-    }
+      const events = await client.paginate(
+        client.issues.listEventsForTimeline,
+        {
+          owner: org,
+          repo: pr.base.repo.name,
+          issue_number: pr.number,
+        }
+      );
 
-    // Merge PRs
-    for (let k = 0; k < prs.length; k++) {
-      const pr = prs[k];
+      if (!(await hasValidMergeLabelActor(pr, events))) {
+        console.log(
+          `PR #${pr.number} has an invalid merge label actor. Skipping...`
+        );
+        continue;
+      }
 
-      // Get PR body w/out comments
-      const prBody = strip(pr.body, {
-        language: "html",
-        preserveNewlines: false,
-      });
-      // Get approvers
-      // Drop [] brackets from PR title
-      // "hello, this is an example [skip-ci][skip-changelog]".replace(/\[[\s\S]*?\]/g, '');
+      // Merge PR
 
-      client.pulls.merge({
-        owner: org,
-        repo: pr.base.repo.name,
-        pull_number: pr.number,
-        merge_method: "squash",
-      });
+      const commitMessage = await createCommitMessage(pr, events);
+      const commitTitle = sanitizePrTitle(pr.title);
+      console.log(`Merging PR#${pr.number} - ${pr.title}`);
+      console.log(`commit title - ${commitTitle}`);
+      console.log(`commit message - ${commitMessage}`);
+
+      // await client.pulls.merge({
+      //   owner: org,
+      //   repo: pr.base.repo.name,
+      //   pull_number: pr.number,
+      //   merge_method: "squash",
+      //   commit_title: commitTitle,
+      //   commit_message: commitMessage,
+      // });
     }
   }
-  console.log("after async");
+  console.log("done!");
 };
 
 if (require.main === module) {
