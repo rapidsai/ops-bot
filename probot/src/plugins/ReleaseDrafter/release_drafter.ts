365e6baa3f81825c434941f5de4bc3f7b841440a
AJ Schmidt <aschmidt@nvidia.com>
2020-11-05 12:56:23 -0500

    use merge base for comparison

--- probot/src/plugins/ReleaseDrafter/release_drafter.ts
+++ probot/src/plugins/ReleaseDrafter/release_drafter.ts
@@ -35,8 +35,9 @@ const draftRelease = async (context: PushContext) => {
   }
 
   const branchVersionNumber: number = parseInt(branchName.split(".")[1]);
-  const compareCommitSHA = await getCompareCommitSHA(
+  const compareCommitSHA = await getMergeBaseSHA(
     context,
+    branchName,
     branchVersionNumber
   );
   const commits = await getRangeCommits(context, compareCommitSHA);
@@ -57,28 +58,31 @@ const draftRelease = async (context: PushContext) => {
 };
 
 /**
- * Returns the SHA of the HEAD commit on the previous release branch
- * (if it exists), otherwise returns an empty string.
+ * Returns the SHA of the merge_base_commit (the common commit between the
+ * current and previous branch). Returns an empty string if no previous branch
+ * exists.
  * @param context
  * @param branchVersionNumber
  */
-const getCompareCommitSHA = async (
+const getMergeBaseSHA = async (
   context: PushContext,
+  branchName: string,
   branchVersionNumber: number
 ): Promise<string> => {
   const owner = context.payload.repository.owner.login;
   const repo = context.payload.repository.name;
   const previousBranchNumber = branchVersionNumber - 1;
-  const previousBranchName = "branch-0." + previousBranchNumber.toString();
+  const previousBranchName = `branch-0.${previousBranchNumber}`;
 
   try {
-    const { data: previousBranchSHA } = await context.github.repos.getBranch({
+    const { data: comparison } = await context.github.repos.compareCommits({
       owner,
       repo,
-      branch: previousBranchName,
+      base: previousBranchName,
+      head: branchName,
     });
 
-    return previousBranchSHA.commit.sha;
+    return comparison.merge_base_commit.sha;
   } catch (error) {
     console.warn(
       `Branch '${previousBranchName}' not found in '${owner}/${repo}'`
