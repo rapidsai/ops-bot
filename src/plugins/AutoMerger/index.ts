e8b1ba56e93cc69ca8a82e48e3e95785b19ae23a
AJ Schmidt <aschmidt@nvidia.com>
2022-03-10 17:21:41 -0500

    rm explicit type

--- src/plugins/AutoMerger/index.ts
+++ src/plugins/AutoMerger/index.ts
@@ -1,11 +1,10 @@
 import { Probot } from "probot";
-import { AutoMergerContext } from "../../types";
 import { AutoMerger } from "./auto_merger";
 
 export const initAutoMerger = (app: Probot) => {
   app.on(
     ["issue_comment.created", "pull_request_review.submitted", "status"],
-    async (context: AutoMergerContext) => {
+    async (context) => {
       await new AutoMerger(context).maybeMergePR();
     }
   );
