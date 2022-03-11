ee18ee307e85fea1b2c6d0d691903e888b51c307
AJ Schmidt <ajschmidt8@users.noreply.github.com>
2022-03-11 11:38:31 -0500

    Fix TypeScript types (#89)
    * fix types & remove ts-ignore statements
    
    * rm explicit type

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,21 @@
 import { Context } from "probot";
 import { Repository } from "@octokit/webhooks-types";
 import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+import {
+  EmitterWebhookEvent,
+  EmitterWebhookEventName,
+} from "@octokit/webhooks";
 
-export type PRContext = Context<"pull_request">;
-export type PushContext = Context<"push">;
-export type IssueCommentContext = Context<"issue_comment">;
-export type PRReviewContext = Context<"pull_request_review">;
-export type StatusContext = Context<"status">;
-export type RepositoryContext = Context<"repository">;
-export type AutoMergerContext = Context<
+type ContextFactory<E extends EmitterWebhookEventName> =
+  EmitterWebhookEvent<E> & Omit<Context, keyof EmitterWebhookEvent>;
+
+export type PRContext = ContextFactory<"pull_request">;
+export type PushContext = ContextFactory<"push">;
+export type IssueCommentContext = ContextFactory<"issue_comment">;
+export type PRReviewContext = ContextFactory<"pull_request_review">;
+export type StatusContext = ContextFactory<"status">;
+export type RepositoryContext = ContextFactory<"repository">;
+export type AutoMergerContext = ContextFactory<
   "issue_comment" | "pull_request_review" | "status"
 >;
 export type PullsGetResponseData =
