42a377850a5aacabd91315573eea8289efe82320
AJ Schmidt <ajschmidt8@users.noreply.github.com>
2023-02-20 00:01:42 -0500

    Add `authorizerFn` function (#132)
    This PR adds a new Lambda function, `authorizerFn`.
    
    The handler for the `authorizerFn` is a simple function that makes use
    of [webhook-authorizer](https://github.com/rapidsai/webhook-authorizer).
    
    The `webhook-authorizer` function accepts a list of organizations that
    are allowed to install the `ops-bot` application.
    
    If the application receives a webhook from an organization that is not
    on the allowed list, it will uninstall the application from that
    organization.
    
    This PR paves the way to enabling a single, publicly installable
    `ops-bot` application that will only be usable by organizations
    specified in the allow list.

---
+++ src/authorizer.ts
@@ -0,0 +1,32 @@
+import { authorizer } from "webhook-authorizer";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const resp = await authorizer({
+    allowedOrgs: ["rapidsai"],
+    event,
+  });
+
+  if (!resp.isAuthorized) {
+    return {
+      statusCode: resp.httpCode,
+      body: resp.msg,
+    };
+  }
+
+  const client = new LambdaClient({});
+  const command = new InvokeCommand({
+    FunctionName: process.env.probotFnName,
+    Payload: Buffer.from(JSON.stringify(event)),
+    InvocationType: "Event",
+  });
+
+  await client.send(command);
+  return {
+    body: "Org is authorized. Webhook received and processing.",
+    statusCode: 202,
+  };
+};
